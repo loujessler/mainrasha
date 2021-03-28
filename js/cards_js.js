1d29b948fd0a46ee590a5d16b8b6ad598a809752
Alex <loujessler@milan-holidays.net>
2021-03-29 02:30:19 +0300

    get to relax :)))))

---
+++ js/cards_js.js
@@ -0,0 +1,6 @@
+// function () {
+//   var value =  getElementById(card)
+//   localStorage.setItem('card', 'value');
+//   var parameter = localStorage.getItem('element_id');
+//   localStorage.removeItem('element_id');
+// }
